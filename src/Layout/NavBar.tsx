30ac6472845e666a63e71aa7f012245edc84992c
yuval miles <yuvalmiles@gmail.com>
2022-10-19 18:15:09 +0300

    schema diff 80%

--- src/Layout/NavBar.tsx
+++ src/Layout/NavBar.tsx
@@ -5,9 +5,26 @@ import Button from "@mui/material/Button";
 import { useAuth } from "../hooks/useAuth";
 import { useState } from "react";
 import ManageDBModal from "./ManageDB/ManageDBModal";
+import { useMutation } from "@tanstack/react-query";
+import axiosClient from "../utils/axiosClient";
+import { useDatabaseStore, DatabaseInfo } from "../store/useDatabaseStore";
+import { AxiosError } from "axios";
 
 const NavBar = () => {
   const { logout } = useAuth();
+  const { selectedDb } = useDatabaseStore((state) => ({
+    selectedDb: state.databaseStore.selectedDatabase,
+  }));
+  const { mutate } = useMutation<any, AxiosError, DatabaseInfo>(
+    async () =>
+      (await axiosClient.post("/database/compare-erd", { id: selectedDb!.id }))
+        .data,
+    {
+      onSuccess: (data) => {
+        console.log(data);
+      },
+    }
+  );
   const [openDBModal, setOpenDBModal] = useState(false);
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -16,6 +33,7 @@ const NavBar = () => {
           <Button variant="outlined" onClick={() => setOpenDBModal(true)}>
             Manage Databases
           </Button>
+          <Button onClick={() => mutate(selectedDb)}>test</Button>
           <Button color="primary" variant="contained" onClick={() => logout()}>
             Logout
           </Button>
